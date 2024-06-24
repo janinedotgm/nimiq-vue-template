a6d5c92aa10571f3635c4e3ee6d95f959bf6069b
Maxi <maximogarciamtnez@gmail.com>
2024-06-24 16:10:24 +0300

    chore: added required dependencies form wasm

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    wasm()
+    wasm(),
+    topLevelAwait(),
   ],
   optimizeDeps: {
     exclude: ['@nimiq/core'],
